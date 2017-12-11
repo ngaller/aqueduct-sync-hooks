ef167fff4d788b52a57230f8ba8c83ece0591f52
Nicolas Galler <ngaller@gmail.com>
2017-12-11 11:45:55 -0500

    Exclude sub properties when creating payload for sync message.

--- addSyncHooks.js
+++ addSyncHooks.js
@@ -1,11 +1,20 @@
 import {MongoInternals} from 'meteor/mongo'
 // const mongodb = MongoInternals.NpmModules.mongodb.module
 
+// remove _id property
 const cleanId = doc => {
   const c = {...doc}
   delete c._id
   return c
 }
+
+// remove dotted properties
+const cleanSubProperties = doc => {
+  return Object.keys(doc).reduce((acc, k) =>
+    k.indexOf('.') < 0 ? Object.assign(acc, doc[k]) : acc, {})
+}
+
+const cleanDocument = doc => cleanSubProperties(cleanId(doc))
 
 // transform Meteor MongoID into a real mongo id atom
 // (this is disabled right now... we just use string as _id)
@@ -16,22 +25,33 @@ const getMongoId = id => id
 module.exports = function addSyncHooks(collection, keyField, type, queue, onError) {
   collection.after.insert(function(userId, doc) {
     queue.add({
-      type: type, data: cleanId(doc), identifier: getMongoId(doc._id), action: 'create'
+      type: type,
+      data: JSON.stringify(cleanDocument(doc)),
+      identifier: getMongoId(doc._id),
+      action: 'create'
     }).catch(onError)
   })
   collection.after.update(function(userId, doc, fieldNames, modifier) {
     if(modifier.$set) {
+      const updateDocument = cleanDocument(modifier.$set)
+      if(Object.keys(updateDocument).length === 0) {
+        // an empty update (they are probably just making updates to the sub properties)
+        return
+      }
       queue.add({
-        type: type, data: {
-          ...modifier.$set,
+        type: type,
+        data: JSON.stringify({
+          ...updateDocument,
           [keyField]: doc[keyField]
-        }, identifier: getMongoId(doc._id), action: 'update'
+        }),
+        identifier: getMongoId(doc._id),
+        action: 'update'
       }).catch(onError)
     }
   })
   collection.after.remove(function(userId, doc) {
     queue.add({
-      type: type, data: doc, action: 'delete'
+      type: type, data: cleanDocument(doc), action: 'delete'
     }).catch(onError)
   })
 }
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nicocrm:aqueduct-sync-hooks',
-  version: '0.0.12',
+  version: '0.0.14',
   // Brief, one-line summary of the package.
   summary: 'Used to add hooks to the meteor collections, for Aqueduct Sync.',
   // URL to the Git repository containing the source code for this package.
